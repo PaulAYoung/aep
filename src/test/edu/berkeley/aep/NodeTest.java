7e5ae913d9c6f7f7ce5bbe1f43863e603022750e
Paul Young <paulandresyoung@gmail.com>
2015-04-10 15:02:44 -0700

    remove duplication

--- src/test/edu/berkeley/aep/NodeTest.java
+++ src/test/edu/berkeley/aep/NodeTest.java
@@ -8,83 +8,59 @@ import static junit.framework.Assert.*;
 /**
  * Created by paul on 4/3/15.
  */
+
+
+
 public class NodeTest {
+
+    static Node a = new Node();
+    static Node b = new Node();
+    static Node c = new Node();
+    static Node d = new Node();
+    static Node e = new Node();
+    static Node f = new Node();
+    static Node g = new Node();
+    static Node h = new Node();
+
+    //Let's just build the whole graph
+    static {
+        a.connect(f);
+        b.connect(c);
+        b.connect(a);
+        c.connect(d);
+        c.connect(e);
+        d.connect(e);
+        e.connect(b);
+        h.connect(b);
+    }
+
     @Test
     public void nodeShouldBeAbleToReachSelf(){
-        Node node = new Node();
-        assertTrue(node.canReach(node));
+        assertTrue(a.canReach(a));
     }
 
     @Test
     public void nodeShouldBeAbleToReachConnected(){
-        Node h = new Node();
-        Node b = new Node();
-        h.connect(b);
         assertTrue(h.canReach(b));
     }
 
     @Test
     public void nodeShouldNotBeAbleToReachUnconnected(){
-        Node h = new Node();
-        Node b = new Node();
-        assertFalse(h.canReach(b));
+        assertFalse(b.canReach(h));
     }
 
     @Test
     public void nodeShouldBeAbleToReachConnectionOfConnected(){
-        Node a = new Node();
-        Node b = new Node();
-        Node c = new Node();
-        a.connect(b);
-        b.connect(c);
-        assertTrue(a.canReach(c));
+        assertTrue(h.canReach(d));
     }
 
     @Test
     public void hShouldReachE(){
-        Node a = new Node();
-        Node b = new Node();
-        Node c = new Node();
-        Node d = new Node();
-        Node e = new Node();
-        Node f = new Node();
-        Node g = new Node();
-        Node h = new Node();
-
-        //Let's just build the whole graph
-        a.connect(f);
-        b.connect(c);
-        b.connect(a);
-        c.connect(d);
-        c.connect(e);
-        d.connect(e);
-        e.connect(b);
-        h.connect(b);
-
         assertTrue(h.canReach(e));
     }
 
     @Test
     public void hShouldNotReachG(){
-        Node a = new Node();
-        Node b = new Node();
-        Node c = new Node();
-        Node d = new Node();
-        Node e = new Node();
-        Node f = new Node();
-        Node g = new Node();
-        Node h = new Node();
-
-        //Let's just build the whole graph
-        a.connect(f);
-        b.connect(c);
-        b.connect(a);
-        c.connect(d);
-        c.connect(e);
-        d.connect(e);
-        e.connect(b);
-        h.connect(b);
-
         assertFalse(h.canReach(g));
     }
 }
